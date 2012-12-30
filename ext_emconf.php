7ad0973dd5c45b982ca76ac0f460da74b6fe7168
Claus Due <claus@wildside.dk>
2012-12-31 00:46:37 +0100

    [TER] 1.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "fluidcontent_fed".
  *
- * Auto generated 16-12-2012 02:31
+ * Auto generated 31-12-2012 00:45
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
-	'version' => '0.9.0',
+	'version' => '1.0.0',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.5-0.0.0',
@@ -44,7 +44,7 @@ $EM_CONF[$_EXTKEY] = array(
 	),
 	'suggests' => array(
 	),
-	'_md5_values_when_last_written' => '',
+	'_md5_values_when_last_written' => 'a:17:{s:12:"ext_icon.gif";s:4:"68b4";s:14:"ext_tables.php";s:4:"d6f2";s:9:"README.md";s:4:"f93e";s:34:"Configuration/TypoScript/setup.txt";s:4:"c886";s:53:"Resources/Private/Elements/Display/AddressAndMap.html";s:4:"621e";s:48:"Resources/Private/Elements/Display/FileList.html";s:4:"85a8";s:54:"Resources/Private/Elements/Flux/AjaxContentLoader.html";s:4:"4856";s:46:"Resources/Private/Elements/Layout/Columns.html";s:4:"72df";s:56:"Resources/Private/Elements/Layout/ContentRandomizer.html";s:4:"4204";s:54:"Resources/Private/Elements/Layout/JQueryAccordion.html";s:4:"c304";s:49:"Resources/Private/Elements/Layout/JQueryTabs.html";s:4:"bc0b";s:53:"Resources/Private/Elements/Navigation/BreadCrumb.html";s:4:"d40f";s:51:"Resources/Private/Elements/Navigation/Redirect.html";s:4:"c075";s:47:"Resources/Private/Elements/Resource/Script.html";s:4:"a062";s:56:"Resources/Private/Elements/Resource/SocialBookmarks.html";s:4:"20ff";s:46:"Resources/Private/Elements/Resource/Style.html";s:4:"f6ea";s:40:"Resources/Private/Language/locallang.xml";s:4:"2552";}',
 );
 
 ?>
