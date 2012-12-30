4e812b51ba1b741e348c39dc0c134a2da03598f9
Claus Due <claus@wildside.dk>
2012-12-30 23:32:26 +0100

    [TASK] Add ext_emconf.php

---
+++ ext_emconf.php
@@ -0,0 +1,50 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "fluidcontent_fed".
+ *
+ * Auto generated 16-12-2012 02:31
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Fluid Content Elements: FED',
+	'description' => 'A collection of general purpose Fluid Content Elements written for EXT:fluidcontent using ViewHelpers from EXT:fed',
+	'category' => 'misc',
+	'author' => 'Claus Due',
+	'author_email' => 'claus@wildside.dk',
+	'author_company' => 'Wildside A/S',
+	'shy' => '',
+	'dependencies' => 'cms,fluidcontent,fed',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'beta',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 1,
+	'lockType' => '',
+	'version' => '2.0.0',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '4.5-0.0.0',
+			'cms' => '',
+			'fluidcontent' => '',
+			'fed' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'suggests' => array(
+	),
+	'_md5_values_when_last_written' => '',
+);
+
+?>
